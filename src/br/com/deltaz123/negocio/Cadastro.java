7f4d17ec6a40249895b2190ba46ca9be428e8330
José Roberto do Patrocínio <jrp2009ba@hotmail.com>
2014-09-12 19:18:50 -0300

    Update da classe Cadastro
    Foram feitas alterações na classe Cadastro.

--- src/br/com/deltaz123/negocio/Cadastro.java
+++ src/br/com/deltaz123/negocio/Cadastro.java
@@ -7,8 +7,57 @@ package br.com.deltaz123.negocio;
 
 /**
  *
- * @author Ruan
+ * @author Patrocínio
  */
-public class Cadastro {
-    
+public class Cadastro{
+    //Declaração de atributos
+    private Equipamento equipamento;
+    private Software software;
+
+    //Construtor
+    public Cadastro(){
+        super();
+    }
+
+    public Cadastro(Equipamento equipamento, Software software){
+        super();
+        this.software = software;
+        this.equipamento = equipamento;
+    }
+
+    //Declaração de métodos
+    public void busca(){
+
+    }
+
+    public void exclusao(){
+
+    }
+
+    public void atualizacao(){
+
+    }
+
+    public void inserir(){
+
+    }
+
+    //Declaração de Getters e Setters
+    public Equipamento getEquipamento(){
+        return equipamento;
+    }
+
+    public void setEquipamento(Equipamento equipamento){
+        this.equipamento = equipamento;
+    }
+
+    public Software getSoftware(){
+        return software;
+    }
+
+    public void setSoftware(Software software){
+        this.software = software;
+    }
+
 }
+
