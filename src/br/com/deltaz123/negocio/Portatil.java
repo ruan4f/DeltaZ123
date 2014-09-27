96b1eff9b4c14956cb90ab91b7a587d56ea660e7
Ruan Ferreira da Silva <ruan4f@gmail.com>
2014-09-27 16:40:03 -0300

    Criado a classe Portatil
    Foi criado a classe portatil.

---
+++ src/br/com/deltaz123/negocio/Portatil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.deltaz123.negocio;
+
+/**
+ *
+ * @author Ruan
+ */
+public class Portatil extends Equipamento{
+  public Fixo (String codEquipamento, String codPatrimonial,
+			String tipo, String accesorio, String tipoUso, String tipoRede) {
+		super();
+		this.setCodEquipamento(codEquipamento);
+		this.setCodPatrimonial(codPatrimonial);
+		this.setTipo (tipo);
+		this.setAccesorio(accesorio);
+		this.setTipoUso(tipoUso);
+		this.setTipoRede (tipoRede);
+	}
+	public Portatil() {
+		super();
+	}
+}
